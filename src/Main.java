072008b7d4de644db6187e78c7e739ca69f171bb
GabrielMenPassos <gabriel.passos3105@gmail.com>
2024-08-08 16:52:51 -0300

    Proxy

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-import prototype.Camisa;
+import prototype.Camisa; // Importa a classe do padrão Prototype
+import proxy.Image;      // Importa a interface do padrão Proxy
+import proxy.ProxyImage; // Importa a classe Proxy do padrão Proxy
+import proxy.RealImage;  // Importa a classe RealImage do padrão Proxy
 
 public class Main {
     public static void main(String[] args) {
+        // Testando o padrão Prototype
         Camisa camisaVermelha = new Camisa("Camisa básica", 29.99, "M", "Vermelha");
-
         Camisa camisaAzul = (Camisa) camisaVermelha.clonar();
 
         System.out.println("Camisa Vermelha:");
@@ -11,5 +14,18 @@ public class Main {
 
         System.out.println("\nCamisa Azul:");
         System.out.println(camisaAzul);
+
+        // Testando o padrão Proxy
+        Image image1 = new ProxyImage("image1.jpg");
+        Image image2 = new ProxyImage("image2.jpg");
+
+        System.out.println("\nDisplaying image1:");
+        image1.display(); // A imagem será carregada e exibida
+
+        System.out.println("\nDisplaying image1 again:");
+        image1.display(); // A imagem já está carregada, não será carregada novamente
+
+        System.out.println("\nDisplaying image2:");
+        image2.display(); // A imagem será carregada e exibida
     }
 }
